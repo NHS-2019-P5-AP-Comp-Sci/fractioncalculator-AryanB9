f6b5614040e6172290983d283a640d6be7c72a83
s-bataana <s-bataana@53S500037011.it.bsd405.org>
2019-11-14 11:24:59 -0800

    Hello

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -1,32 +1,65 @@
+
 /**
  * @author Mr. Rasmussen
  */
-
 package fracCalc;
+
+import java.util.*;
 
 public class FracCalc {
 
-    public static void main(String[] args)
-    {
-        // TODO: Read the input from the user and call produceAnswer with an equation
+	public static void main(String[] args) {
+		// TODO: Read the input from the user and call produceAnswer with an equation
+		System.out.println("Welcome to Fraction Calculator");
+		Scanner intro = new Scanner(System.in);
+		System.out.print("Enter a fraction problem: ");
+		String userResponse = intro.nextLine();
+		System.out.println(produceAnswer(userResponse));
+	}
 
-    }
+	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
+	// test your code
+	// This function takes a String 'input' and produces the result
+	//
+	// input is a fraction string that needs to be evaluated. For your program, this
+	// will be the user input.
+	// e.g. input ==> "1/2 + 3/4"
+	//
+	// The function should return the result of the fraction after it has been
+	// calculated
+	// e.g. return ==> "1_1/4"
+	public static String produceAnswer(String input) {
+		String operand1 = "";
+		String operator = "";
+		String operand2 = "";
 
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
-        // TODO: Implement this function to produce the solution to the input
+		String layout = "";
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) == ' ') {
+				if (operand1.equals("")) {
+					operand1 = layout;
+					layout = "";
 
-        return "";
-    }
+				} else {
+					operator = layout;
+					layout = "";
+				}
+			} else if (i == input.length() - 1) {
+				layout += input.charAt(i);
+				operand2 = layout;
+			} else {
+				layout += input.charAt(i);
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
+			}
+
+		}
+		return operand2;
+
+		// TODO: Implement this function to produce the solution to the input
+
+	}
 
 }
+
+// TODO: Fill in the space below with any helper methods that you think you will
+// need
